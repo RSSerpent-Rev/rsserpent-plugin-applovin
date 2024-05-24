a6a367be7f0ec4eb75f1fea0e3b1f963f5610958
EkkoG <beijiu572@gmail.com>
2024-05-24 10:47:16 +0800

    chore: rename plugin

--- rsserpent_plugin_applovin_sdk_update/__init__.py
+++ rsserpent_plugin_applovin_sdk_update/__init__.py
@@ -4,13 +4,13 @@ from . import route
 
 
 plugin = Plugin(
-    name="rsserpent-plugin-applovin",
+    name="rsserpent-plugin-applovin-sdk-update",
     author=Persona(
         name="EkkoG",
         link="https://github.com/EkkoG",
         email="beijiu572@gmail.com",
     ),
-    prefix="/applovin/sdk",
+    prefix="/applovin/sdk-update",
     repository="https://github.com/RSSerpent-Rev/rsserpent-plugin-applovin",
     routers={route.path: route.provider},
 )
--- tests/test_route.py
+++ tests/test_route.py
@@ -2,7 +2,7 @@ from starlette.testclient import TestClient
 
 
 def test_route(client: TestClient) -> None:
-    """Test `rsserpent_plugin_applovin.route`."""
+    """Test `rsserpent_plugin_applovin_sdk_update.route`."""
     response = client.get("/applovin")
     assert response.status_code == 200
     assert response.headers["Content-Type"] == "application/xml"
